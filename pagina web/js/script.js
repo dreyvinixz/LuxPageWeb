1831d17d0e1884ddd43982c11a829ef72bd921a1
dreyvinixz <andreyvini062@gmail.com>
2024-11-25 19:32:06 -0300

    rename directory

--- pagina web/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const listaItens = document.querySelectorAll('.menu .list_guia li');
-
-    listaItens.forEach(function(item) {
-        item.addEventListener('click', function() {
-            this.classList.toggle('open');
-        });
-    });
-});
